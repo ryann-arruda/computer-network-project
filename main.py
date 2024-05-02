670dbbb68787d38dcd25ee865203569374fdb33e
Alexandreprog <alexandrebezerra3207@gmail.com>
2024-05-02 11:33:19 -0300

    custom menu created Closed #2

---
+++ main.py
@@ -0,0 +1,17 @@
+option = -1
+
+while option != 4:
+    print("Escolha o tipo de Requisição que deseja realizar")
+    print("1 - Data e hora atual")
+    print("2 - Uma mensagem motivacional para o fim do semestre")
+    print("3 - A quantidade de respostas emitidas pelo servidor até o momento")
+    print("4 - Sair")
+
+    option = int(input("Digite a opção desejada: "))
+
+    if option == 1:
+        print("opção 1")
+    if option == 2:
+        print("opção 2")
+    if option == 3:
+        print("opção 3")  
